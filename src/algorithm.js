71ffadedb4e87b413bdc941084ba36f27b874205
higton <matpaulo@hotmail.com.br>
2022-03-05 18:20:05 -0300

    update

--- src/algorithm.js
+++ src/algorithm.js
@@ -1,18 +1,24 @@
-
 (function main() {
-    function knapSack(values, weights, i, target) {
-        if(target < 0) return Number.MIN_SAFE_INTEGER;
-        if(i < 0 || target === 0) return 0;
+    function knapSack(values, weights, i, target, result) {
+        if(target < 0) return [Number.MIN_SAFE_INTEGER, result.slice()];
+        if(i < 0 || target === 0) return [0, result.slice()];
 
-        let take = knapSack(values, weights, i - 1, target - weights[i]) + values[i];
-        let not_take = knapSack(values, weights, i - 1, target);
+        result.push(values[i])
+        let take = knapSack(values, weights, i - 1, target - weights[i], result);
+        take[0] += values[i]
+        result.pop()
+        let not_take = knapSack(values, weights, i - 1, target, result);
 
-        return Math.max(take, not_take);
+        if (take[0] > not_take[0]) {
+            return take
+        }
+        
+        return not_take
     }
 
     let weights = [1,2,3,4]
     let values = [50,200,150,100]
     let capacity = 7
 
-    console.log(knapSack(values, weights, values.length - 1, capacity)); 
-}());
+    console.log(knapSack(values, weights, values.length - 1, capacity, [])); 
+}()); 
