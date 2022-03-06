509606e340c862ddd4f3fb4bf10c2831925b7f61
higton <matpaulo@hotmail.com.br>
2022-03-06 17:52:54 -0300

    update output

--- src/algorithm.js
+++ src/algorithm.js
@@ -3,7 +3,7 @@
         if(target < 0) return [Number.MIN_SAFE_INTEGER, result.slice()];
         if(i < 0 || target === 0) return [0, result.slice()];
 
-        result.push(values[i])
+        result.push(i)
         let take = knapSack(values, weights, i - 1, target - weights[i], result);
         take[0] += values[i]
         result.pop()
@@ -16,9 +16,13 @@
         return not_take
     }
 
+    let names = ['test1', 'test2', 'test3', 'test4']
     let weights = [1,2,3,4]
     let values = [50,200,150,100]
     let capacity = 7
-
-    console.log(knapSack(values, weights, values.length - 1, capacity, [])); 
+    let result = knapSack(values, weights, values.length - 1, capacity, []); 
+    
+    return result[1].map((index) => {
+        return {nome: names[index], valor: values[index], peso: weights[index]}
+    })
 }()); 
